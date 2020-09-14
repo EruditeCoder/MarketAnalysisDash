bcd7c2cac6fc6e8be9d0ed5d3ebe1b77b2903cbd
EruditeCoder <marcosaleman84@gmail.com>
2020-09-13 22:38:20 -0400

    Changed API call

--- components/DataFetcher.js
+++ components/DataFetcher.js
@@ -13,17 +13,12 @@ function DOLJoblessClaimsFetcher() {
 
 
 function BARDXYFetcher() {
-    // let quoteURL = 'https://marketdata.websol.barchart.com/getHistory.json?apikey=' + process.env.BAR_API_KEY + '&symbol=TLT&type=daily&startDate=20200608&endDate=20200912&order=asc'
+    let quoteURL = 'https://marketdata.websol.barchart.com/getHistory.json?apikey=' + process.env.BAR_API_KEY + '&symbol=TLT&type=daily&startDate=20200608&endDate=20200912&order=asc'
 
-    // fetch(quoteURL)
-    // .then(function (response) {
-    //     console.log(response)
-    // })
-
-    console.log("BAR API KEY:")
-    console.log(process.env.BAR_API_KEY)
-    console.log("BLS API KEY:")
-    console.log(process.env.BLS_API_KEY)
+    fetch(quoteURL)
+    .then(function (response) {
+        console.log(response)
+    })
 }
 
-BARDXYFetcher()
+// BARDXYFetcher()
